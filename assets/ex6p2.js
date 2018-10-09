4b7460d703f89437479d7408baf5b839cbb01763
abdel <a.naji@hotmail.fr>
2018-10-09 16:11:11 +0200

    ex6p2

---
+++ assets/ex6p2.js
@@ -0,0 +1,6 @@
+function changeImage(image){
+document.getElementById(image).src='assets/img/'+image+'_2.jpg';
+}
+function originImage(image){
+document.getElementById(image).src='assets/img/'+image+'.jpg';
+}
